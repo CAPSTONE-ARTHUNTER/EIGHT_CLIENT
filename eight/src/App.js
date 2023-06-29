01b69a8dd7de64cb19000c4feecab0350736cbdc
wrryu09 <rryu09@ewhain.net>
2023-06-29 21:31:42 +0900

    ✨ feat: pages, Routes

--- eight/src/App.js
+++ eight/src/App.js
@@ -1,135 +1,22 @@
-import axios from "axios";
-// import fs from "fs";
-import { useEffect } from "react";
-import React, { useState, useRef } from "react";
-import { Camera } from "react-camera-pro";
-import styled from "styled-components";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-const Component = () => {
-  const camera = useRef(null);
-  const [numberOfCameras, setNumberOfCameras] = useState(0);
-  const [image, setImage] = useState();
-  const [detected, setDetected] = useState(['Detect']);
-  useEffect(() => {
-    console.log("detected!", detected);
-  }, [detected]);
-
-  useEffect(() => {
-    if (image) {
-      infer();
-    }
-  }, [image]);
-
-  var infer = function () {
-    var detectedList = [];
-    console.log("infer!!");
-    setDetected([]);
-    if(image===undefined){
-      console.log('image is undefined.');
-    }
-    axios({
-      method: "POST",
-      url: "https://detect.roboflow.com/painting-l6exb/9",
-      params: {
-        api_key: process.env.REACT_APP_ROBOFLOW_API, //env처리
-      },
-      data: image,
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-    })
-    .then(function (response) {
-      console.log(response.data);
-      if (response.data.predictions.length !== 0) {
-        setDetected(prevDetectedList => {
-          const updatedDetectedList = [...prevDetectedList];
-          for (var i = 0; i < response.data.predictions.length; i++) {
-            console.log(response.data.predictions[i].class);
-            updatedDetectedList.push(response.data.predictions[i].class);
-          }
-          return updatedDetectedList;
-        });
-      } else {
-        setDetected(prevDetectedList => [...prevDetectedList, "none"]);
-      }
-      cleanArray();
-      })
-      .catch(function (error) {
-        console.log(error.message);
-        detectedList = ["error"];
-        setDetected(detectedList);
-        cleanArray();
-      });
-  };
-
-  async function takeaPic() {
-      const photoTaken = await camera.current.takePhoto();
-    setImage(photoTaken);
-  }
-
-  function cleanArray() {
-    setTimeout(() => {
-      setDetected(['Detect']);
-      console.log("clean!");
-    }, 1500);
-  }
-
-  return (
-    <div>
-      <Camera
-        ref={camera}
-        numberOfCamerasCallback={setNumberOfCameras}
-        aspectRatio={1 / 1}
-        facingMode="environment"
-      />
-      <button onClick={() => takeaPic()}>Take photo</button>
-      <button
-        // hidden={numberOfCameras <= 1}
-        onClick={() => {
-          camera.current.switchCamera();
-        }}
-      >
-        switchCamera
-      </button>
-      <div>
-        {detected.length === 0 ? (
-          <div>
-            <TitleText>detecting...</TitleText>
-          </div>
-        ) : (
-          <div>
-            {detected.map((data) => (
-              <TitleText key={data}>{data}</TitleText>
-            ))}
-          </div>
-        )}
-      </div>
-      <img src={image} height={300} width={300} alt="Taken photo" />
-    </div>
-  );
-};
+import MainPage from "./pages/MainPage";
+import Detection from "./pages/Detection";
+import Collection from "./pages/Collection";
+import SearchPage from "./pages/SearchPage";
+import MyPage from "./pages/MyPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <script src="https://cdn.roboflow.com/0.2.20/roboflow.js"></script>
-        <TitleText>Eight🎱</TitleText>
-        <CamContainer>
-          <Component></Component>
-        </CamContainer>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/collection" element={<Collection />} />
+      <Route path="/detection" element={<Detection />} />
+      <Route path="/search" element={<SearchPage />} />
+      <Route path="/mypage" element={<MyPage />} />
+    </Routes>
   );
 }
-
-const TitleText = styled.text`
-  font-size: 40pt;
-`;
-const CamContainer = styled.div`
-  height: 300px;
-  width: 300px;
-  background: black;
-`;
 
 export default App;
---
+++ eight/src/pages/Detection.js
@@ -0,0 +1,135 @@
+import React from "react";
+import axios from "axios";
+import { useEffect } from "react";
+import { useState, useRef } from "react";
+import { Camera } from "react-camera-pro";
+import styled from "styled-components";
+
+const Component = () => {
+  const camera = useRef(null);
+  const [numberOfCameras, setNumberOfCameras] = useState(0);
+  const [image, setImage] = useState();
+  const [detected, setDetected] = useState(["Detect"]);
+  useEffect(() => {
+    console.log("detected!", detected);
+  }, [detected]);
+
+  useEffect(() => {
+    if (image) {
+      infer();
+    }
+  }, [image]);
+
+  var infer = function () {
+    var detectedList = [];
+    console.log("infer!!");
+    setDetected([]);
+    if (image === undefined) {
+      console.log("image is undefined.");
+    }
+    axios({
+      method: "POST",
+      url: "https://detect.roboflow.com/painting-l6exb/9",
+      params: {
+        api_key: process.env.REACT_APP_ROBOFLOW_API, //env처리
+      },
+      data: image,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    })
+      .then(function (response) {
+        console.log(response.data);
+        if (response.data.predictions.length !== 0) {
+          setDetected((prevDetectedList) => {
+            const updatedDetectedList = [...prevDetectedList];
+            for (var i = 0; i < response.data.predictions.length; i++) {
+              console.log(response.data.predictions[i].class);
+              updatedDetectedList.push(response.data.predictions[i].class);
+            }
+            return updatedDetectedList;
+          });
+        } else {
+          setDetected((prevDetectedList) => [...prevDetectedList, "none"]);
+        }
+        cleanArray();
+      })
+      .catch(function (error) {
+        console.log(error.message);
+        detectedList = ["error"];
+        setDetected(detectedList);
+        cleanArray();
+      });
+  };
+
+  async function takeaPic() {
+    const photoTaken = await camera.current.takePhoto();
+    setImage(photoTaken);
+  }
+
+  function cleanArray() {
+    setTimeout(() => {
+      setDetected(["Detect"]);
+      console.log("clean!");
+    }, 1500);
+  }
+
+  return (
+    <div>
+      <Camera
+        ref={camera}
+        numberOfCamerasCallback={setNumberOfCameras}
+        aspectRatio={1 / 1}
+        facingMode="environment"
+      />
+      <button onClick={() => takeaPic()}>Take photo</button>
+      <button
+        // hidden={numberOfCameras <= 1}
+        onClick={() => {
+          camera.current.switchCamera();
+        }}
+      >
+        switchCamera
+      </button>
+      <div>
+        {detected.length === 0 ? (
+          <div>
+            <TitleText>detecting...</TitleText>
+          </div>
+        ) : (
+          <div>
+            {detected.map((data) => (
+              <TitleText key={data}>{data}</TitleText>
+            ))}
+          </div>
+        )}
+      </div>
+      <img src={image} height={300} width={300} alt="Taken photo" />
+    </div>
+  );
+};
+
+function Detection(props) {
+  return (
+    <div>
+      <header className="App-header">
+        <script src="https://cdn.roboflow.com/0.2.20/roboflow.js"></script>
+        <TitleText>Eight🎱</TitleText>
+        <CamContainer>
+          <Component></Component>
+        </CamContainer>
+      </header>
+    </div>
+  );
+}
+
+const TitleText = styled.text`
+  font-size: 40pt;
+`;
+const CamContainer = styled.div`
+  height: 300px;
+  width: 300px;
+  background: black;
+`;
+
+export default Detection;
