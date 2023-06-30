bd2ced58473389b60b66853d752461de2d4fa122
wrryu09 <rryu09@ewhain.net>
2023-06-30 15:41:28 +0900

    💄 add assets

---
+++ eight/src/assets/icon/index.js
@@ -0,0 +1,10 @@
+// BottomBar
+export {ReactComponent as CameraIco} from './bottomBar/Camera.svg';
+export {ReactComponent as BookmarkIco} from './bottomBar/Bookmark.svg';
+export {ReactComponent as HomeIco} from './bottomBar/Home.svg';
+export {ReactComponent as MypageIco} from './bottomBar/Person.svg';
+export {ReactComponent as SearchIco} from './bottomBar/Search.svg';
+
+// mainPage
+export { ReactComponent as CollectionIco } from "./mainPage/collectionIco.svg";
+export { ReactComponent as DocentIco } from "./mainPage/docentIco.svg";
