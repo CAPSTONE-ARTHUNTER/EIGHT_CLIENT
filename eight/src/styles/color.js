c77111d20caee1030c8f1e94493cb0a353f05b12
wrryu09 <rryu09@ewhain.net>
2023-06-30 15:42:18 +0900

    💄 add color, fonts

---
+++ eight/src/styles/color.js
@@ -0,0 +1,11 @@
+export const colors = {
+  black: "#2C2927",
+  lightGrey: "#F3F3F3",
+  beige: "#CFC8BC",
+  copper1: "#A79C8D",
+  copper2: "#9B8D7B",
+  brown: "#534C49",
+  redBrown: "#9F6648",
+  orange: "#FFBA35",
+  white: "#FFFFFF",
+};
---
+++ eight/src/styles/typo.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import { colors } from "./color";
+import "./font.css";
+
+const Title01 = styled.text`
+  font-size: 24px;
+  font-family: Pretendard;
+  font-style: normal;
+  font-weight: 800;
+  line-height: normal;
+  color: ${(props) => (props.color ? props.color : colors.black)};
+`;
+
+const Body01 = styled.text`
+  font-size: 12px;
+  font-family: Pretendard;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 140%;
+  letter-spacing: -0.24px;
+  color: ${(props) => (props.color ? props.color : colors.black)};
+`;
+const Body02 = styled.text`
+  font-size: 14px;
+  font-family: Pretendard;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  color: ${(props) => (props.color ? props.color : colors.black)};
+`;
+
+const typo = {
+  title: {
+    Title01: Title01,
+  },
+  body: {
+    Body01: Body01,
+    Body02: Body02,
+  },
+};
+
+export default typo;
