4a5935ff32854719d90043b319b82e4a3963d66e
wrryu09 <rryu09@ewhain.net>
2023-07-20 03:24:05 +0900

    💄 add Layout bottom margin

--- eight/src/components/Layout/Layout.js
+++ eight/src/components/Layout/Layout.js
@@ -11,11 +11,13 @@ const Layout = (props) => {
       <SizedBox height={52} />
       {props.children}
       <BottomBar />
+      <SizedBox height={56} />
     </Container>
   );
 };
 const Container = styled.div`
   margin-left: 16px;
   margin-right: 16px;
+  margin-bottom: 32px;
 `;
 export default Layout;
