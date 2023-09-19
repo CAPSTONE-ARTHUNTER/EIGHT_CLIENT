463950e9dd228478c1e8ab1d1f9bbd1cdc9dfc37
wrryu09 <rryu09@ewhain.net>
2023-09-19 13:20:42 +0900

    📦 add permission_handler

--- eight_client/lib/main.dart
+++ eight_client/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:eight_client/webview.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-void main() {
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Permission.camera.request();
 
   runApp(MaterialApp(
     home: HomeScreen(),
