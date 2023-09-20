2b6f977e22e8f81b62985ba56dee6cd394548efd
wrryu09 <rryu09@ewhain.net>
2023-09-20 21:26:10 +0900

    ✨ webview 적용 (안드)
    flutter_inappwebview 패키지 사용, camera permission request

--- eight_client/lib/main.dart
+++ eight_client/lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:eight_client/webview.dart';
+import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 Future main() async {
@@ -7,7 +9,60 @@ Future main() async {
 
   await Permission.camera.request();
 
-  runApp(MaterialApp(
-    home: HomeScreen(),
-  ));
+  if (Platform.isAndroid) {
+    await AndroidInAppWebViewController.setWebContentsDebuggingEnabled(true);
+  }
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: InAppWebViewPage());
+  }
+}
+
+class InAppWebViewPage extends StatefulWidget {
+  @override
+  _InAppWebViewPageState createState() => new _InAppWebViewPageState();
+}
+
+class _InAppWebViewPageState extends State<InAppWebViewPage> {
+  late InAppWebViewController _webViewController;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+            body: Container(
+                child: Column(children: <Widget>[
+      Expanded(
+        child: Container(
+          child: InAppWebView(
+              initialUrlRequest: URLRequest(
+                  url: Uri.parse("https://capstone-eight.netlify.app/")),
+              initialOptions: InAppWebViewGroupOptions(
+                crossPlatform: InAppWebViewOptions(
+                  mediaPlaybackRequiresUserGesture: false,
+                ),
+              ),
+              onWebViewCreated: (InAppWebViewController controller) {
+                _webViewController = controller;
+              },
+              androidOnPermissionRequest: (InAppWebViewController controller,
+                  String origin, List<String> resources) async {
+                return PermissionRequestResponse(
+                    resources: resources,
+                    action: PermissionRequestResponseAction.GRANT);
+              }),
+        ),
+      ),
+    ]))));
+  }
 }
