62a89af8665ecf475d8321e06224f8c939f60a4e
wrryu09 <rryu09@ewhain.net>
2023-10-05 20:06:04 +0900

    ✨ 퍼즐 페이지 data 서버 연결 #27

--- eight/src/components/docent/GetBadgeInfo.js
+++ eight/src/components/docent/GetBadgeInfo.js
@@ -4,14 +4,14 @@ import { badge_wolf } from "../../assets/image/badge";
 import typo from "../../styles/typo";
 import SizedBox from "../Common/SizedBox";
 
-const GetBadgeInfo = ({ done, leftPart, badgeName, t }) => {
+const GetBadgeInfo = ({ done, leftPart, badge, artName, t }) => {
   return (
     <Container>
       <img src={badge_wolf} alt="wolfBadge" className="badge" />
       <SizedBox height={20} />
       {done ? (
         <>
-          <typo.body.Body02>{badgeName}</typo.body.Body02>
+          <typo.body.Body02>{badge}</typo.body.Body02>
           <typo.body.Body02>{t("DocentPage.badgeComplete")}</typo.body.Body02>
         </>
       ) : (
@@ -20,7 +20,7 @@ const GetBadgeInfo = ({ done, leftPart, badgeName, t }) => {
             {t("DocentPage.badgeInfo1", { leftPart: leftPart })}
           </typo.body.Body02>
           <typo.body.Body02>
-            {t("DocentPage.badgeInfo2", { badgeName: badgeName })}
+            {t("DocentPage.badgeInfo2", { badgeName: artName })}
           </typo.body.Body02>
         </>
       )}
