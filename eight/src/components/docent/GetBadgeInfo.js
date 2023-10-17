18dbbacecb494b6ff166bb8d3bfa13641b9196ce
wrryu09 <rryu09@ewhain.net>
2023-10-17 13:14:31 +0900

    🐛 badge 연결 수정

--- eight/src/components/docent/GetBadgeInfo.js
+++ eight/src/components/docent/GetBadgeInfo.js
@@ -1,17 +1,16 @@
 import React from "react";
 import styled from "styled-components";
-import { badge_wolf } from "../../assets/image/badge";
 import typo from "../../styles/typo";
 import SizedBox from "../Common/SizedBox";
 
 const GetBadgeInfo = ({ done, leftPart, badge, artName, t }) => {
   return (
     <Container>
-      <img src={badge_wolf} alt="wolfBadge" className="badge" />
+      <img src={badge} alt="artBadge" className="badge" />
       <SizedBox height={20} />
       {done ? (
         <>
-          <typo.body.Body02>{badge}</typo.body.Body02>
+          <typo.body.Body02>{artName}</typo.body.Body02>
           <typo.body.Body02>{t("DocentPage.badgeComplete")}</typo.body.Body02>
         </>
       ) : (
